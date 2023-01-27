cfc5cf3dbac13c5f2eff7533438487f252e4bb23
thaohoangc3 <thaohoang@remitano.com>
2023-01-27 14:35:52 +0700

    add unit test for set pool creator

---
+++ sdk/tests/integration/set_pool_creator_authority.test.ts
@@ -0,0 +1,55 @@
+import * as anchor from "@project-serum/anchor";
+import * as assert from "assert";
+import { toTx, WhirlpoolContext, WhirlpoolIx, WhirlpoolsConfigData } from "../../src";
+import { generateDefaultConfigParams } from "../utils/test-builders";
+
+describe("set_pool_creator_authority", () => {
+  const provider = anchor.AnchorProvider.local();
+  anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.Whirlpool;
+  const ctx = WhirlpoolContext.fromWorkspace(provider, program);
+  const fetcher = ctx.fetcher;
+
+  it("successfully set_pool_creator_authority", async () => {
+    const {
+      configInitInfo,
+    } = generateDefaultConfigParams(ctx);
+    const defaultPoolCreatorWallet = ctx.wallet
+    await toTx(ctx, WhirlpoolIx.initializeConfigIx(ctx.program, configInitInfo)).buildAndExecute();
+    const newAuthorityKeypair = anchor.web3.Keypair.generate();
+    await toTx(
+      ctx,
+      WhirlpoolIx.setPoolCreatorAuthorityIx(ctx.program, {
+        whirlpoolsConfig: configInitInfo.whirlpoolsConfigKeypair.publicKey,
+        poolCreatorAuthority: defaultPoolCreatorWallet.publicKey,
+        newPoolCreatorAuthority: newAuthorityKeypair.publicKey,
+      })
+    )
+      .buildAndExecute();
+
+    const config = (await fetcher.getConfig(
+      configInitInfo.whirlpoolsConfigKeypair.publicKey
+    )) as WhirlpoolsConfigData;
+    assert.ok(config.poolCreatorAuthority.equals(newAuthorityKeypair.publicKey));
+  });
+
+  it("fails if invalid set_pool_creator_authority provided", async () => {
+    const { configInitInfo } = generateDefaultConfigParams(ctx);
+    await toTx(ctx, WhirlpoolIx.initializeConfigIx(ctx.program, configInitInfo)).buildAndExecute();
+    const otherAuthorityKeypair = anchor.web3.Keypair.generate();
+
+    await assert.rejects(
+      toTx(
+        ctx,
+        WhirlpoolIx.setPoolCreatorAuthorityIx(ctx.program, {
+          whirlpoolsConfig: configInitInfo.whirlpoolsConfigKeypair.publicKey,
+          poolCreatorAuthority: otherAuthorityKeypair.publicKey,
+          newPoolCreatorAuthority: provider.wallet.publicKey,
+        })
+      )
+      .addSigner(otherAuthorityKeypair)
+      .buildAndExecute(),
+      /0x7dc/ // An address constraint was violated
+    );
+  });
+});
