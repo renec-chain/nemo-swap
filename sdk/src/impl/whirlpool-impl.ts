2e3731280c30db4969f4484b0f69f6090931467c
thaohoangc3 <thaohoang@remitano.com>
2023-02-03 11:19:47 +0700

    add whirlpool pub to closePosition sdk

--- sdk/src/impl/whirlpool-impl.ts
+++ sdk/src/impl/whirlpool-impl.ts
@@ -558,6 +558,7 @@ export class WhirlpoolImpl implements Whirlpool {
 
     /* Close position */
     const positionIx = closePositionIx(this.ctx.program, {
+      whirlpool: positionData.whirlpool,
       positionAuthority: positionWallet,
       receiver: destinationWallet,
       positionTokenAccount,
