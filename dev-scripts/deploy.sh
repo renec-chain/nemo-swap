80c0d2f769dc686e005de4e18e831605e5869ef8
kientt6034 <kientrinh@remitano.com>
2023-06-05 17:11:35 +0700

    scripts: add dev scripts

---
+++ dev-scripts/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ -n $1 ]]; then
+    WALLET_NAME=$1
+else
+    echo "Please provide a wallet name"
+    exit 1
+fi
+
+PROGRAM_NAME_UNDERSCORE=${PROGRAM_NAME//-/_}
+solana program deploy target/deploy/$PROGRAM_NAME_UNDERSCORE.so --keypair .wallets/$WALLET_NAME.json --url $CLUSTER_URL
