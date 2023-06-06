5378073596a04769d648230b8a8d86ad1b3d179f
kientt6034 <kientrinh@remitano.com>
2023-06-07 01:06:49 +0700

    perf: update init fee tier script

--- scripts/create_pool/01_init_fee_tier.ts
+++ scripts/create_pool/01_init_fee_tier.ts
@@ -8,7 +8,6 @@ import {
   FeeTierData,
 } from "@renec/redex-sdk";
 import { loadProvider, loadWallets } from "./utils";
-import config from "./config.json";
 import { configEnv } from "../env.config";
 import deployed from "./deployed.json";
 
@@ -21,49 +20,40 @@ async function main() {
   }
   const { ctx } = loadProvider(wallets.feeAuthKeypair);
 
-  if (configEnv.REDEX_CONFIG_PUB_KEY === "") {
-    console.log(
-      "ReDEX Pool Config is not found. Please run `npm run 00-create-pool-config` ."
-    );
-    return;
-  }
-  const REDEX_CONFIG_PUB = new PublicKey(deployed.REDEX_CONFIG_PUB);
+  const REDEX_CONFIG_PUB = new PublicKey(configEnv.REDEX_CONFIG_PUB_KEY);
   const configAccount = (await ctx.fetcher.getConfig(
     REDEX_CONFIG_PUB
   )) as WhirlpoolsConfigData;
 
-  if (configAccount) {
-    for (let i = 0; i < config.FEE_TIERS.length; i++) {
-      const feeTier = config.FEE_TIERS[i];
-      const feeTierPda = PDAUtil.getFeeTier(
-        ctx.program.programId,
-        REDEX_CONFIG_PUB,
-        feeTier.TICK_SPACING
-      );
-      let feeTierAccount = (await ctx.fetcher.getFeeTier(
-        feeTierPda.publicKey
-      )) as FeeTierData;
-      if (feeTierAccount) {
-        printFeeTier(feeTierPda.publicKey, feeTierAccount);
-        continue;
-      }
-      console.log("deploying fee tier account...");
-      const params: InitFeeTierParams = {
-        feeTierPda,
-        whirlpoolsConfig: REDEX_CONFIG_PUB,
-        tickSpacing: feeTier.TICK_SPACING,
-        defaultFeeRate: feeTier.DEFAULT_FEE_RATE,
-        feeAuthority: configAccount.feeAuthority,
-        funder: ctx.wallet.publicKey,
-      };
-      const tx = toTx(
-        ctx,
-        WhirlpoolIx.initializeFeeTierIx(ctx.program, params)
-      ).addSigner(wallets.feeAuthKeypair);
-      const txid = await tx.buildAndExecute();
-      console.log("fee tier account deployed at txid:", txid);
-    }
+  const feeTierPda = PDAUtil.getFeeTier(
+    ctx.program.programId,
+    REDEX_CONFIG_PUB,
+    configEnv.FEE_TIERS_TICK_SPACING
+  );
+  let feeTierAccount = (await ctx.fetcher.getFeeTier(
+    feeTierPda.publicKey
+  )) as FeeTierData;
+  if (feeTierAccount) {
+    printFeeTier(feeTierPda.publicKey, feeTierAccount);
+    return;
   }
+
+  console.log("deploying fee tier account...");
+  const params: InitFeeTierParams = {
+    feeTierPda,
+    whirlpoolsConfig: REDEX_CONFIG_PUB,
+    tickSpacing: configEnv.FEE_TIERS_TICK_SPACING,
+    defaultFeeRate: configEnv.FEE_TIERS_DEFAULT_FEE_RATE,
+    feeAuthority: configAccount.feeAuthority,
+    funder: ctx.wallet.publicKey,
+  };
+
+  const tx = toTx(
+    ctx,
+    WhirlpoolIx.initializeFeeTierIx(ctx.program, params)
+  ).addSigner(wallets.feeAuthKeypair);
+  const txid = await tx.buildAndExecute();
+  console.log("fee tier account deployed at txid:", txid);
 }
 
 function printFeeTier(publicKey: PublicKey, feeTierAccount: FeeTierData) {
