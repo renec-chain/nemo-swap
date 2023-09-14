46605a2cde70803b31c4b801d891501c20b428cd
kientt6034 <kientrinh@remitano.com>
2023-09-14 17:25:09 +0700

    feat: discount fee when swap with fee discount

--- sdk/src/instructions/composites/swap-async.ts
+++ sdk/src/instructions/composites/swap-async.ts
@@ -2,6 +2,7 @@ import { resolveOrCreateATAs, TransactionBuilder, ZERO } from "@orca-so/common-s
 import { PublicKey } from "@solana/web3.js";
 import { SwapUtils, TickArrayUtil, Whirlpool, WhirlpoolContext } from "../..";
 import { SwapInput, swapIx } from "../swap-ix";
+import { swapWithFeeDiscountIx } from "../swap_with_fee_discount";
 
 export type SwapAsyncParams = {
   swapInput: SwapInput;
@@ -55,6 +56,56 @@ export async function swapAsync(
     swapIx(
       ctx.program,
       SwapUtils.getSwapParamsFromQuote(
+        swapInput,
+        ctx,
+        whirlpool,
+        inputTokenAccount,
+        outputTokenAccount,
+        wallet
+      )
+    )
+  );
+}
+
+export async function swapWithFeeDiscountAsync(
+  ctx: WhirlpoolContext,
+  params: SwapAsyncParams,
+  refresh: boolean
+): Promise<TransactionBuilder> {
+  const { wallet, whirlpool, swapInput } = params;
+  const { aToB, amount } = swapInput;
+  const txBuilder = new TransactionBuilder(ctx.connection, ctx.wallet);
+  const tickArrayAddresses = [swapInput.tickArray0, swapInput.tickArray1, swapInput.tickArray2];
+
+  let uninitializedArrays = await TickArrayUtil.getUninitializedArraysString(
+    tickArrayAddresses,
+    ctx.fetcher,
+    refresh
+  );
+  if (uninitializedArrays) {
+    throw new Error(`TickArray addresses - [${uninitializedArrays}] need to be initialized.`);
+  }
+
+  const data = whirlpool.getData();
+  const [resolvedAtaA, resolvedAtaB] = await resolveOrCreateATAs(
+    ctx.connection,
+    wallet,
+    [
+      { tokenMint: data.tokenMintA, wrappedSolAmountIn: aToB ? amount : ZERO },
+      { tokenMint: data.tokenMintB, wrappedSolAmountIn: !aToB ? amount : ZERO },
+    ],
+    () => ctx.fetcher.getAccountRentExempt()
+  );
+  const { address: ataAKey, ...tokenOwnerAccountAIx } = resolvedAtaA;
+  const { address: ataBKey, ...tokenOwnerAccountBIx } = resolvedAtaB;
+  txBuilder.addInstructions([tokenOwnerAccountAIx, tokenOwnerAccountBIx]);
+  const inputTokenAccount = aToB ? ataAKey : ataBKey;
+  const outputTokenAccount = aToB ? ataBKey : ataAKey;
+
+  return txBuilder.addInstruction(
+    swapWithFeeDiscountIx(
+      ctx.program,
+      SwapUtils.getSwapWithFeeDiscountParamsFromQuote(
         swapInput,
         ctx,
         whirlpool,
