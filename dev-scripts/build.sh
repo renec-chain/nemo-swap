1a455ef3b88d98cc4b02b46970a34250e6763d68
kientt6034 <kientrinh@remitano.com>
2023-10-25 19:39:40 +0700

    fix: makefile, updatae renec mainnet version

--- dev-scripts/build.sh
+++ dev-scripts/build.sh
@@ -36,7 +36,7 @@ else
 fi
 
 # Get program id from the file
-UPDATED_PROGRAM_ID=$(grep -E 'declare_id!\("[A-Za-z0-9]{44}"\);' "$FILE_PATH" | grep -oE '[A-Za-z0-9]{44}')
+UPDATED_PROGRAM_ID=$(grep -E 'declare_id!\("[A-Za-z0-9]{43,44}"\);' "$FILE_PATH" | grep -oE '[A-Za-z0-9]{43,44}')
 echo "updated program id: $UPDATED_PROGRAM_ID"
 
 # Compare it to the existing PROGRAM_ID
--- sdk/src/quotes/swap/tick-array-sequence.ts
+++ sdk/src/quotes/swap/tick-array-sequence.ts
@@ -123,6 +123,8 @@ export class TickArraySequence {
   findNextInitializedTickIndex(currIndex: number) {
     const searchIndex = this.aToB ? currIndex : currIndex + this.tickSpacing;
     let currTaIndex = TickArrayIndex.fromTickIndex(searchIndex, this.tickSpacing);
+    console.log("search index: ", searchIndex);
+    console.log("currTaIndex: ", currTaIndex.arrayIndex);
 
     // Throw error if the search attempted to search for an index out of bounds
     if (!this.isArrayIndexInBounds(currTaIndex, this.aToB)) {
@@ -165,6 +167,10 @@ export class TickArraySequence {
   private isArrayIndexInBounds(index: TickArrayIndex, aToB: boolean) {
     // a+0...a+n-1 array index is ok
     const localArrayIndex = this.getLocalArrayIndex(index.arrayIndex, aToB);
+    if (index.arrayIndex == 1) {
+      console.log("localArrayIndex: ", localArrayIndex);
+      console.log("this.sequence.length: ", this.sequence.length);
+    }
     const seqLength = this.sequence.length;
     return localArrayIndex >= 0 && localArrayIndex < seqLength;
   }
