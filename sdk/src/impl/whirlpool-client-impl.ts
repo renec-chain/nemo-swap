3f4e5076620bdc9b57cd4e688b2dc11f388ceace
Lợi Trà <trathailoi@gmail.com>
2023-12-05 08:49:42 +0700

    chore: support exact-out swap

--- sdk/src/impl/whirlpool-client-impl.ts
+++ sdk/src/impl/whirlpool-client-impl.ts
@@ -348,22 +348,25 @@ export class WhirlpoolClientImpl implements WhirlpoolClient {
     const whirlpoolData1 = whirlpool1.getData();
     const whirlpoolData2 = whirlpool2.getData();
 
+    const quote1NativeTokenAmount = swapQuote1.amountSpecifiedIsInput ? swapQuote1.amount : swapQuote1.otherAmountThreshold
+    const quote2NativeTokenAmount = swapQuote2.amountSpecifiedIsInput ? swapQuote2.amount : swapQuote2.otherAmountThreshold
+
     const requests = [
       {
         tokenMint: whirlpoolData1.tokenMintA,
-        wrappedSolAmountIn: swapQuote1.aToB ? swapQuote1.amount : ZERO,
+        wrappedSolAmountIn: swapQuote1.aToB ? quote1NativeTokenAmount : ZERO,
       },
       {
         tokenMint: whirlpoolData1.tokenMintB,
-        wrappedSolAmountIn: !swapQuote1.aToB ? swapQuote1.amount : ZERO,
+        wrappedSolAmountIn: !swapQuote1.aToB ? quote1NativeTokenAmount : ZERO,
       },
       {
         tokenMint: whirlpoolData2.tokenMintA,
-        wrappedSolAmountIn: swapQuote2.aToB ? swapQuote2.amount : ZERO,
+        wrappedSolAmountIn: swapQuote2.aToB ? quote2NativeTokenAmount : ZERO,
       },
       {
         tokenMint: whirlpoolData2.tokenMintB,
-        wrappedSolAmountIn: !swapQuote2.aToB ? swapQuote2.amount : ZERO,
+        wrappedSolAmountIn: !swapQuote2.aToB ? quote2NativeTokenAmount : ZERO,
       },
     ];
 
