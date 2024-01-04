3f4988855c64ce10dce73f18a58ff31a7c2bbf29
kientt6034 <kientrinh@remitano.com>
2024-01-05 02:12:54 +0700

    feat: create wrenec ata for swap tx

--- sdk/src/instructions/composites/swap-async.ts
+++ sdk/src/instructions/composites/swap-async.ts
@@ -1,8 +1,9 @@
-import { resolveOrCreateATAs, TransactionBuilder, ZERO } from "@orca-so/common-sdk";
+import { Instruction, resolveOrCreateATAs, TransactionBuilder, ZERO } from "@orca-so/common-sdk";
 import { PublicKey } from "@solana/web3.js";
 import { SwapUtils, TickArrayUtil, Whirlpool, WhirlpoolContext } from "../..";
 import { SwapInput, swapIx } from "../swap-ix";
 import { swapWithFeeDiscountIx } from "../swap_with_fee_discount";
+import { NATIVE_MINT } from "@solana/spl-token";
 
 export type SwapAsyncParams = {
   swapInput: SwapInput;
@@ -65,6 +66,99 @@ export async function swapAsync(
       )
     )
   );
+}
+
+/**
+ * Swap instruction builder method with resolveATA & additional checks.
+ * @param ctx - WhirlpoolContext object for the current environment.
+ * @param params - {@link SwapAsyncParams}
+ * @param refresh - If true, the network calls will always fetch for the latest values.
+ * @returns
+ */
+export async function swapAsyncWithWRenecAta(
+  ctx: WhirlpoolContext,
+  params: SwapAsyncParams,
+  refresh: boolean,
+  wRenecAta?: PublicKey
+): Promise<{ tx: TransactionBuilder; createdWRenecAta: PublicKey | undefined }> {
+  const { wallet, whirlpool, swapInput } = params;
+  const { aToB, amount } = swapInput;
+  const txBuilder = new TransactionBuilder(ctx.connection, ctx.wallet);
+  const tickArrayAddresses = [swapInput.tickArray0, swapInput.tickArray1, swapInput.tickArray2];
+
+  let uninitializedArrays = await TickArrayUtil.getUninitializedArraysString(
+    tickArrayAddresses,
+    ctx.fetcher,
+    refresh
+  );
+  if (uninitializedArrays) {
+    throw new Error(`TickArray addresses - [${uninitializedArrays}] need to be initialized.`);
+  }
+
+  const data = whirlpool.getData();
+
+  let request = [
+    { tokenMint: data.tokenMintA, wrappedSolAmountIn: aToB ? amount : ZERO },
+    { tokenMint: data.tokenMintB, wrappedSolAmountIn: !aToB ? amount : ZERO },
+  ];
+
+  const [resolvedAtaA, resolvedAtaB] = await resolveOrCreateATAs(
+    ctx.connection,
+    wallet,
+    request,
+    () => ctx.fetcher.getAccountRentExempt()
+  );
+
+  let { address: ataAKey, ...tokenOwnerAccountAIx } = resolvedAtaA;
+  let { address: ataBKey, ...tokenOwnerAccountBIx } = resolvedAtaB;
+
+  let instructions: Instruction[] = [tokenOwnerAccountAIx, tokenOwnerAccountBIx];
+
+  let createdWRenecAta: PublicKey | undefined = undefined;
+
+  // if a is token mint
+  if (data.tokenMintA.equals(NATIVE_MINT)) {
+    if (wRenecAta) {
+      createdWRenecAta = wRenecAta;
+      ataAKey = wRenecAta;
+
+      // remove first instruction
+      instructions.shift();
+    } else {
+      createdWRenecAta = ataAKey;
+    }
+  } else if (data.tokenMintB.equals(NATIVE_MINT)) {
+    if (wRenecAta) {
+      createdWRenecAta = wRenecAta;
+      ataBKey = wRenecAta;
+
+      // remove second instruction
+      instructions.pop();
+    } else {
+      createdWRenecAta = ataBKey;
+    }
+  }
+
+  txBuilder.addInstructions([tokenOwnerAccountAIx, tokenOwnerAccountBIx]);
+  const inputTokenAccount = aToB ? ataAKey : ataBKey;
+  const outputTokenAccount = aToB ? ataBKey : ataAKey;
+
+  return {
+    tx: txBuilder.addInstruction(
+      swapIx(
+        ctx.program,
+        SwapUtils.getSwapParamsFromQuote(
+          swapInput,
+          ctx,
+          whirlpool,
+          inputTokenAccount,
+          outputTokenAccount,
+          wallet
+        )
+      )
+    ),
+    createdWRenecAta,
+  };
 }
 
 export async function swapWithFeeDiscountAsync(
